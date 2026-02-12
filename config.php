6196ed4e775f89d38957b234f3d449715b7bed15
Gaurang <gaurang.rathod9591@gmail.com>
2026-02-12 15:29:48 +0530

    objectfs config changes

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ $CFG->dboptions = array(
 
 // Site settings
 $CFG->wwwroot   = getenv('MOODLE_SITE_URL') ?: 'https://cinnalab-moodle-d1962611ca61.herokuapp.com';
-$CFG->dataroot = '/tmp/moodledata';
+$CFG->dataroot = '/app/moodledata';
 $CFG->admin     = 'admin';
 
 // S3 File Storage Configuration using objectfs tool
