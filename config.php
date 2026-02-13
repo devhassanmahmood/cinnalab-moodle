4c15f9d97ca5480634b470e7b296445d507db0fe
Gaurang <gaurang.rathod9591@gmail.com>
2026-02-13 21:18:03 +0530

    moodle file upload to s3 fixes custom plugin

--- config.php
+++ config.php
@@ -54,7 +54,8 @@ $CFG->directorypermissions = 02777;
 // $CFG->debugemail = '';
 // $CFG->debugpageinfo = getenv('MOODLE_DEBUG_PAGEINFO') ? 1 : 0;
 // @error_reporting(E_ALL | E_STRICT);
-// @ini_set('display_errors', '1');
+@ini_set('display_errors', '0');
+@ini_set('log_errors', '1');
 // $CFG->debug = (E_ALL | E_STRICT);
 // $CFG->debugdisplay = 1;
 // Cache settings
